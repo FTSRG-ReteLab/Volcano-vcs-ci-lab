e9c7eb008151c43cff3d50d9d2ccd0fa3f21f416
danielkrausz <dnielkrausz@gmail.com>
2020-04-03 02:22:19 +0200

    Implement reference speed change by following joystick movements

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -4,60 +4,61 @@ import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
-	private int step = 0;
-	private int referenceSpeed = 0;
-	private int speedLimit = 0;
-	private Thread thread;
+    private int step = 0;
+    private int referenceSpeed = 0;
+    private int speedLimit = 0;
+    private Thread thread;
 
-	public TrainControllerImpl() { thread = new Thread() {
-			public void run() {
-				thread.run();
-				try {
-					followSpeed();
-					thread.sleep(200);
-				} catch(Exception e) {	
-				System.out.println(e.getMessage());	
-				}				
-			}
-		};		
-	}
-	@Override
-	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
-			System.out.println("Nullára állt a sebesség");
-		} else {
-		    if(referenceSpeed+step > 0) {
+    public TrainControllerImpl() {
+        thread = new Thread() {
+            public void run() {
+                thread.start();
+                try {
+                    followSpeed();
+                    thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void followSpeed() {
+        if (referenceSpeed < 0) {
+            referenceSpeed = 0;
+        } else {
+            if (referenceSpeed + step > 0) {
                 referenceSpeed += step;
             } else {
-		        referenceSpeed = 0;
+                referenceSpeed = 0;
             }
-		}
+        }
 
-		enforceSpeedLimit();
-	}
+        enforceSpeedLimit();
+    }
 
-	@Override
-	public int getReferenceSpeed() {
-		return referenceSpeed;
-	}
+    @Override
+    public int getReferenceSpeed() {
+        return referenceSpeed;
+    }
 
-	@Override
-	public void setSpeedLimit(int speedLimit) {
-		this.speedLimit = speedLimit;
-		enforceSpeedLimit();
-		
-	}
+    @Override
+    public void setSpeedLimit(int speedLimit) {
+        this.speedLimit = speedLimit;
+        enforceSpeedLimit();
 
-	private void enforceSpeedLimit() {
-		if (referenceSpeed > speedLimit) {
-			referenceSpeed = speedLimit;
-		}
-	}
+    }
 
-	@Override
-	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
-	}
+    private void enforceSpeedLimit() {
+        if (referenceSpeed > speedLimit) {
+            referenceSpeed = speedLimit;
+        }
+    }
+
+    @Override
+    public void setJoystickPosition(int joystickPosition) {
+        this.step = joystickPosition;
+    }
 
 }
